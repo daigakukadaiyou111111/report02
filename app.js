2159d9a375305aa144d712c72e6edc923e143933
test <test@test>
2024-08-01 20:08:32 +0900

    report

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var yesnoRouter = require('./routes/yesno');
-var notesRouter = require('./routes/notes');
+// var notesRouter = require('./routes/notes');
 var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/yesno', yesnoRouter);
-app.use('/notes', notesRouter); 
+// app.use('/notes', notesRouter); 
 app.use('/notes_from_b', notes_from_bRouter);
   
 
